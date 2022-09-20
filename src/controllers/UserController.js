ee2961a7a3d62746879b6a7d1a4647d0e7bad453
Dharm-8434 <dharm15121998@gmail.com>
2022-09-20 14:49:03 +0530

    validation donefor usercrater

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,21 +6,21 @@ const createUser = async function (req, res) {
   try {
     let data = req.body
     
-    // if (data.title == undefined) {
-    //   return res.status(401).send({ msg: "fname Compulsory" })
-    // }
-    // if (data.name == undefined) {
-    //   return res.status(401).send({ msg: "lname Compulsory" })
-    // }
-    // if (!(/^[a-z0-9_]{3,}@[a-z]{3,}.[a-z]{3,6}$/).test(data.emailId)) {
-    //   return res.status(400).send({ status: false, message: `Email should be a valid email address` });
-    // }
-    // if (data.password == undefined) {
-    //   return res.status(401).send({ msg: "password Compulsory" })  
-    // }
-    // if (data.phone == undefined) {
-    //   return res.status(401).send({ msg: "title Compulsory" })
-    // }
+     if (data.title == undefined) {
+       return res.status(401).send({ msg: "title Compulsory" })
+     }
+    if (data.name == undefined) {
+      return res.status(401).send({ msg: "name Compulsory" })
+    }
+    if (!(/^[a-z0-9_]{3,}@[a-z]{3,}.[a-z]{3,6}$/).test(data.email)) {
+      return res.status(400).send({ status: false, message: `Email should be a valid email address` });
+    }
+    if (data.password == undefined) {
+      return res.status(401).send({ msg: "password Compulsory" })  
+    }
+    if (data.phone == undefined) {
+      return res.status(401).send({ msg: "Phone Number Compulsory" })
+    }
     let savedData = await userModel.create(data)
     return res.status(201).send({ msg: savedData })
   }
